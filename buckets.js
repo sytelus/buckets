4f675b2272760b1fac706be222e4d1bb1da94e50
Shital Shah <shital@ShitalShah.com>
2013-11-21 00:58:04 -0800

    Added AMD/requireJS support

--- buckets.js
+++ buckets.js
@@ -2383,4 +2383,12 @@ var buckets = {};
     if (typeof module !== 'undefined') {
         module.exports = buckets;
     }
+	
+	// Make AMD/RequireJS compatible
+	if ( typeof define === "function" && define.amd ) {
+		define( "jquery", [], function() {
+			return jQuery;
+		});
+	}	
+	
 }());
